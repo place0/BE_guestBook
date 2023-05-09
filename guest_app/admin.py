f46e4355c1ce870404bb2c9e990ab311a1e4ab98
place0 <thdud030101@naver.com>
2023-05-10 04:41:25 +0900

    모델 수정

--- guest_app/admin.py
+++ guest_app/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import GuestBook
 
-#class GuestBookAdmin(admin.ModelAdmin):
-#    list_display = ('title', 'author', 'content', 'date')
-
-#admin.site.register(GuestBook, GuestBookAdmin)
 
 admin.site.register(GuestBook)
+
--- guest_app/models.py
+++ guest_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class GuestBook(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=10)
-    content = models.TextField
+    content = models.TextField(null=True)
     datetime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
